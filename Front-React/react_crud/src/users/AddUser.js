4632e249eaa21078b005cf5595a5c17d750d8c45
sabbir <absabbir1@gmail.com>
2023-01-15 23:15:11 +0600

    Post added

--- Front-React/react_crud/src/users/AddUser.js
+++ Front-React/react_crud/src/users/AddUser.js
@@ -1,7 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function AddUser() {
+export default function Adddepartment() {
+  let navigate = useNavigate();
+
+  const [department, setdepartment] = useState({
+    departmentName: "",
+    departmentCode: "",
+    departmentAddress: "",
+  });
+
+  const { departmentName, departmentCode, departmentAddress } = department;
+
+  const onInputChange = (e) => {
+    setdepartment({ ...department, [e.target.name]: e.target.value });
+    
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:1000/departments", department);
+    navigate("/");
+  };
   return (
-    <div>AddUser Form</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 border mt-2 offset-md-3 p-4 shadow">
+          <h2 className="text-center m-4">Register Department</h2>
+
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Department Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your Department Name"
+                name="departmentName"
+                value={departmentName}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="code" className="form-label">
+                Department Code
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your Department Code"
+                name="departmentCode"
+                value={departmentCode}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="address" className="form-label">
+                Department Address
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your Department Address"
+                name="departmentAddress"
+                value={departmentAddress}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button className="btn btn-outline-success" type="submit">
+              Submit
+            </button>
+            <button className="btn btn-outline-danger mx-3" type="submit">
+              Cancel
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
