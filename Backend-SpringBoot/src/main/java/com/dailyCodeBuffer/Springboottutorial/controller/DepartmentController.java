b107df9aa0500e7bcc9524ae0798eb2c3ce1451b
sabbir <absabbir1@gmail.com>
2023-01-15 19:38:21 +0600

    Routing to Add User page

--- Backend-SpringBoot/src/main/java/com/dailyCodeBuffer/Springboottutorial/controller/DepartmentController.java
+++ Backend-SpringBoot/src/main/java/com/dailyCodeBuffer/Springboottutorial/controller/DepartmentController.java
@@ -6,6 +6,7 @@ import com.dailyCodeBuffer.Springboottutorial.service.DepartmentService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class DepartmentController {
 
     @Autowired
